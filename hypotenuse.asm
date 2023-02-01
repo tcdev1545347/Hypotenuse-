af1cc958e892a73a36e9b6ed1815019509f88078
tcdev1545347 <112838818+tcdev1545347@users.noreply.github.com>
2023-02-01 11:11:16 -0800

    Update hypotenuse.asm

--- hypotenuse.asm
+++ hypotenuse.asm
@@ -1,3 +1,16 @@
+;****************************************************************************************************************************
+;   Program name: "Hypotenuse".  This program takes in the user input for 2 sides in float                                     
+;   calculates the hypotenuse. Copyright (C) 2021 Tien Cao.                                                                    
+;                                                                                                                           
+;   This file is part of the software program "Hypotenuse".                                                                    
+;   Hypotenuse is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License    
+;   version 3 as published by the Free Software Foundation.                                                                    
+;   Hypotenuse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied           
+;   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     
+;   A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.   
+;                         *
+;****************************************************************************************************************************
+
 ;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
 ;
 ;Author information
@@ -6,16 +19,17 @@
 ;
 ;Program information
 ;  Program name: Hypotenuse
-;  Programming languages: One modules in C++ and one module in X86
+;  Programming languages: One modules in C++, one module in X86, and one module in Bash.
 ;  Date program began: 2023 February 1
 ;  Date of last update: 2023 February 6
-;
 ;  Files in this program: pythagoras.cpp, hypotenuse.asm
-;
+;  Status: This program has been tested by the author
 ;This file
 ;   File name: hypotenuse.asm
 ;   Language: X86 with Intel syntax.
 ;   Assemble: nasm -f elf64 -l hypotenuse.lis -o hypotenuse.o hypotenuse.asm
+;   Purpose: Calculating the hypotenuse of a triangle using float inputs. And output floats in assembly
+;   Limitations: Input validation 
 
 ;===== Begin code area ================================================================================================
 
